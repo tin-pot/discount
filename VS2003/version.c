3108951d432b0e389acdc7d50b1af38745c19bd0
tin-pot <tin-pot@gmx.net>
2012-01-02 04:23:02 +0100

    Added MSVC .NET 2003 project files and pre-configured config.h, mkdio.h, version.c, blocktags.

---
+++ VS2003/version.c
@@ -0,0 +1,37 @@
+/*
+ * version.c -- Version of markdown for MSVC / Win32.
+ */
+
+#include "config.h"
+
+#define XSTR(s) #s
+#define STR(s) XSTR(s)
+
+char markdown_version[] = VERSION
+#if TABSTOP != 4
+                " TAB=" STR(TABSTOP)
+#endif
+#if USE_AMALLOC
+		" DEBUG"
+#endif
+#if USE_DISCOUNT_DL
+# if USE_EXTRA_DL
+		" DL=BOTH"
+# else
+		" DL=DISCOUNT"
+# endif
+#elif USE_EXTRA_DL
+		" DL=EXTRA"
+#else
+		" DL=NONE"
+#endif
+#if WITH_ID_ANCHOR
+		" ID-ANCHOR"
+#endif
+#if WITH_GITHUB_TAGS
+		" GITHUB-TAGS"
+#endif
+#if WITH_FENCED_CODE
+		" FENCED-CODE"
+#endif
+		;
